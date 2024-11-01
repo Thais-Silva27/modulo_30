2369b5411ee548f0c92ba719131fa999632c95b1
Thais Silva <141892213+Thais-Silva27@users.noreply.github.com>
2024-11-01 12:58:30 -0300

    Add files via upload

---
+++ src/containers/Produtos.tsx
@@ -0,0 +1,19 @@
+import Produto from '../components/Produto'
+import { useGetLojaQuery } from '../services/api'
+import * as S from './styles'
+
+const ProdutosComponent = () => {
+  const { data: produtos, isLoading } = useGetLojaQuery()
+
+  if (isLoading) return <h2>Carregando...</h2>
+
+  return (
+    <S.Produtos>
+      {produtos?.map((produto) => (
+        <Produto key={produto.id} produto={produto} />
+      ))}
+    </S.Produtos>
+  )
+}
+
+export default ProdutosComponent
